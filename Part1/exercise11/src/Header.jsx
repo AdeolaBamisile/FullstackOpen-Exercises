4edfe39f401a850cf63f38523c6b7affa6832615
AdeolaBamisile <theacutesniper@gmail.com>
2026-03-21 12:21:58 +0100

    Completed exercise 1.1

---
+++ Part1/exercise11/src/Header.jsx
@@ -0,0 +1,5 @@
+const Header = (props) => {
+  return <div>{props.Header}</div>;
+};
+
+export default Header;
---
+++ Part1/exercise11/src/Total.jsx
@@ -0,0 +1,5 @@
+const Total = (props) => {
+  return <div>Number of exercises {props.Total}</div>;
+};
+
+export default Total;
