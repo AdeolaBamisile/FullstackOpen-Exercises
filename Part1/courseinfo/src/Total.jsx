f300092aba624adf10c682e856856b0ea1f14329
AdeolaBamisile <theacutesniper@gmail.com>
2026-03-22 18:15:41 +0100

    Completed exercise 1.4

--- Part1/courseinfo/src/Total.jsx
+++ Part1/courseinfo/src/Total.jsx
@@ -1,5 +1,12 @@
 const Total = (props) => {
-  return <div>Number of exercises {props.Total}</div>;
+  return (
+    <div>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </div>
+  );
 };
 
 export default Total;
