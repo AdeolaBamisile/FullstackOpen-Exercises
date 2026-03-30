02472170b302915688d39de5cc38e3533bbc9793
AdeolaBamisile <theacutesniper@gmail.com>
2026-03-31 00:45:56 +0100

    Completed Exercise 1.9

--- Part1/unicafe/src/Statistics.jsx
+++ Part1/unicafe/src/Statistics.jsx
@@ -1,14 +1,18 @@
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  return (
-    <>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {average}</div>
-      <div>positive {positive} %</div>
-    </>
-  );
+  if (all == 0) {
+    return <div>No feedback given</div>;
+  } else {
+    return (
+      <>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {average}</div>
+        <div>positive {positive} %</div>
+      </>
+    );
+  }
 };
 
 export default Statistics;
