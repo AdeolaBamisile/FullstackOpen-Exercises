1e88266e685c62f4d96acdecca36fa34fa14c5d7
AdeolaBamisile <theacutesniper@gmail.com>
2026-05-09 17:37:24 +0100

    Pushing code to deploy backend on Render

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (request, response) => {
     return JSON.stringify(request.body)
@@ -76,7 +78,7 @@ app.post('/api/persons', (request, response) => {
     response.json(body)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
